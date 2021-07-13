40d14d60933717ef832e427a2f1f3c82aebab2ed
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-13 13:06:12 -0700

    Removed custome variable in place of prewritten variable

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ var $title = document.querySelector('#note-title');
 var $urlInput = document.querySelector('#url');
 var $notes = document.querySelector('#entry-notes');
 var $submit = document.querySelector('button');
-var notes = [];
-
+var oldEntries;
 function setImage(event) {
 
   document.querySelector('img').setAttribute('src', $urlInput.value);
@@ -14,28 +13,31 @@ function setImage(event) {
 $urlInput.addEventListener('input', setImage);
 
 function resetForm(event) {
-  notes.push({
+  data.entries = oldEntries.entries;
+  data.entries.push({
     title: $title.value,
     photoURL: $urlInput.value,
     notes: $notes.value
   });
+  data.nextEntryId = oldEntries.nextEntryId;
+  data.nextEntryId += 1;
 
-  $title.value = '';
-  $urlInput.value = '';
-  $notes.value = '';
+  document.querySelector('#note.title').reset();
+  document.querySelector('#url').reset();
+  document.querySelector('#entry-notes').reset();
   document.querySelector('img').setAttribute('src', 'images/placeholder-image-square.jpg');
-  return notes;
+  return data;
 }
 
 $submit.addEventListener('click', resetForm);
 
 function storeData(event) {
-
-  return localStorage.setItem('notepages', JSON.stringify(notes));
+  return localStorage.setItem('notepages', JSON.stringify(data));
 
 }
-var oldEntries = localStorage.getItem('notepages');
+oldEntries = localStorage.getItem('notepages');
 if (oldEntries !== null) {
-  notes = JSON.parse(oldEntries);
+  oldEntries = JSON.parse(oldEntries);
 }
+
 window.addEventListener('beforeunload', storeData);

7f6b21534ce451d59bcb7f4c5409f5344e94f9b2
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-14 13:11:51 -0700

    In styles.css I created a new ruleset for bottom-margin for the image because the bottom text was hugging the image. They are spaced evenly now. I also created a list margin to do the exact same thing both those rulesets have to different sets of margin so I wasn't able to reuse it. In main.js I created a function to create dom elements and to create list of the entries. I made a for loop to target the entry id and append those entries (starting from the newest) to the dom.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 /* global data */
 /* exported data */
+
+// User can create new entry *START*
 var $title = document.querySelector('#note-title');
 var $urlInput = document.querySelector('#url');
 var $notes = document.querySelector('#entry-notes');
@@ -18,12 +20,44 @@ function resetForm(event) {
     title: $title.value,
     photoURL: $urlInput.value,
     notes: $notes.value,
-    entryId: data.entries.length
+    entryId: data.nextEntryId
   });
-  data.nextEntryId = data.entries.length;
+  data.nextEntryId++;
   $submit.reset();
   document.querySelector('img').setAttribute('src', 'images/placeholder-image-square.jpg');
 
 }
 
 $submit.addEventListener('submit', resetForm);
+// New entry *END*
+
+// For Dom Creation *START*
+function newEntryCreation(entries, idNumber) {
+  var listItem = document.createElement('li');
+  listItem.className = 'entry-bottom-margin';
+  var listFlex = document.createElement('div');
+  listFlex.className = 'justify-between row';
+  var entryImage = document.createElement('img');
+  entryImage.className = 'column-half image-padding square-image image-bottom-margin';
+  var columnHalfDiv = document.createElement('div');
+  columnHalfDiv.className = 'column-half';
+  var entryTitle = document.createElement('h3');
+  entryTitle.className = 'entry-title sans-serif input-title-block-margin-reset title-margin';
+  var entryNotes = document.createElement('p');
+  entryNotes.className = 'input-font-size sans-serif';
+
+  listItem.appendChild(listFlex);
+  listFlex.appendChild(entryImage);
+  listFlex.appendChild(columnHalfDiv);
+  columnHalfDiv.appendChild(entryTitle);
+  columnHalfDiv.appendChild(entryNotes);
+  entryImage.setAttribute('src', data.entries[idNumber].photoURL);
+  entryTitle.textContent = data.entries[idNumber].title;
+  entryNotes.textContent = data.entries[idNumber].notes;
+  return listItem;
+}
+var entryList = document.querySelector('ul');
+for (var i = data.entries[data.entries.length - 1].entryId; i >= 0; i--) {
+  entryList.appendChild(newEntryCreation(data.entries, i));
+}
+// Dom Creation *END*
